07de90a53457c02a1e193de3048263cc65f8c236
DY <p930601@gmail.com>
2023-01-16 13:02:22 +0700

    ✅ FEAT : Put / Patch / Delete cats data API

--- LetsStart/src/cats/cats.router.ts
+++ LetsStart/src/cats/cats.router.ts
@@ -1,4 +1,5 @@
 // READ : 고양이 전체 데이터 조회 API
+import * as express from "express";
 import { Cat, CatType } from "./app.model";
 import { Router } from "express";
 
@@ -34,6 +35,59 @@ router.post("/cats", (req, res) => {
     const data = req.body;
     Cat.push(data);
     res.status(200).send({ success: true, data: { data } });
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// UPDATE (PUT) : 고양이 데이터 전체 업데이트 API
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = body;
+        result = cat;
+      }
+    });
+
+    res.status(200).send({ success: true, data: { cat: result } });
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// UPDATE (PATCH) : 고양이 데이터 부분 업데이트 API
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = { ...cat, ...body }; // 객체 구조 분해 할당
+        result = cat;
+      }
+    });
+
+    res.status(200).send({ success: true, data: { cat: result } });
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// DELETE : 고양이 데이터 삭제 API
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+
+    const newCat = Cat.filter((cat) => cat.id !== params.id);
+
+    res.status(200).send({ success: true, data: { cat: newCat } });
   } catch (error) {
     res.status(400).send({ success: false, error: error.message });
   }
