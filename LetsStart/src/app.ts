2c6cc051048e2bfa92d258540d7198b3fd47c54f
DY <p930601@gmail.com>
2023-01-16 12:54:44 +0700

    ✅ CHORE : npm install express

--- LetsStart/src/app.ts
+++ LetsStart/src/app.ts
@@ -1,14 +1,29 @@
-import * as express from 'express';
-import { Cat, CatType } from './app.model';
+import { ExecOptionsWithBufferEncoding } from "child_process";
+import * as express from "express";
+import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 // const app: express.Application = express()
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req);
+// 미들웨어
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("middleware");
+  next();
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat });
 });
 
-app.listen(3000, () => {
-  console.log('server on');
+app.get("/cats/blue", (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.listen(8000, () => {
+  console.log("server on");
 });
