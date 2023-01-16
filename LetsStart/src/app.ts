e8f4f84a9aa845e4880fd58cd3d94c9bf09e844f
DY <p930601@gmail.com>
2023-01-16 12:57:22 +0700

    ✅ FEAT : All/Specific cat data inquiry API

--- LetsStart/src/app.ts
+++ LetsStart/src/app.ts
@@ -4,13 +4,38 @@ import { Cat, CatType } from "./app.model";
 const app: express.Express = express();
 // const app: express.Application = express()
 
-// logging
+// logging middleware
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
   console.log("This is logging middleware");
   next();
 });
 
+// READ : 고양이 전체 데이터 조회 API
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    // throw new Error('db connect error'); // 에러 강제 발생
+    res.status(200).send({ success: true, data: { cats } });
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// READ : 특정 고양이 데이터 조회 API
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const cats = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({ success: true, data: { cats } });
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// 404 middleware
 app.use((req, res, next) => {
   console.log("This is error middleware");
   res.send({ error: "404 Not found error" });
